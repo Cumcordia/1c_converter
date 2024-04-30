556d4f85483d605283c5af03bb243ed10186eea6
Cumcordia <maksimmakarov228666@gmail.com>
2024-04-30 17:31:37 +0500

    v0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            richTextBox3 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 95);
+            button1.Name = "button1";
+            button1.Size = new Size(159, 38);
+            button1.TabIndex = 2;
+            button1.Text = "Конвертировать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(177, 95);
+            button2.Name = "button2";
+            button2.Size = new Size(159, 38);
+            button2.TabIndex = 3;
+            button2.Text = "История";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 19);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(239, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 48);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(239, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 153);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(159, 234);
+            richTextBox1.TabIndex = 6;
+            richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(178, 153);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(306, 234);
+            richTextBox2.TabIndex = 7;
+            richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.Location = new Point(490, 153);
+            richTextBox3.Name = "richTextBox3";
+            richTextBox3.Size = new Size(298, 234);
+            richTextBox3.TabIndex = 8;
+            richTextBox3.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(richTextBox3);
+            Controls.Add(richTextBox2);
+            Controls.Add(richTextBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private RichTextBox richTextBox3;
+    }
+}
